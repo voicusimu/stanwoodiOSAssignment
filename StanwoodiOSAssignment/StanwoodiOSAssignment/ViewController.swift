ab4bd684ef365c3790a63d1ee6ea553aec7a18af
Simu Voicu-Mircea <voicu.simu@hotmail.com>
2020-09-23 13:03:25 +0300

    Initial project creation

---
+++ StanwoodiOSAssignment/StanwoodiOSAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StanwoodiOSAssignment
+//
+//  Created by Simu Voicu-Mircea on 23/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
